943aa7defbc65851ffd7c274bf3c4173717bc4ca
riho1074 <riho1074@colorado.edu>
2024-04-01 13:08:54 -0600

    Required Routes

--- app.py
+++ app.py
@@ -28,3 +28,49 @@ def db_create():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgres://database_example_lff5_user:64t2vX6bUfBZ0f6XMZZTZzkyzYjgn0F5@dpg-co5en5fsc6pc7385cnvg-a/database_example_lff5")
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO Basketball (First, Last, City, Name, Number)
+    Values
+    ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+    ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+    ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+    ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgres://database_example_lff5_user:64t2vX6bUfBZ0f6XMZZTZzkyzYjgn0F5@dpg-co5en5fsc6pc7385cnvg-a/database_example_lff5")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+        records = cur.fetchall()
+        conn.close()
+        response_string=""
+        response_string+="<table>"
+        for player in records:
+            response_string+="<tr>"
+            for info in player:
+                response_string+="<td>{}</td>".format(info)
+            response_string+="</tr>"
+        response_string+="<table>"
+        return response_string
+
+@app.route('/dp_drop')
+def dropping():
+    conn = psycopg2.connect("postgres://database_example_lff5_user:64t2vX6bUfBZ0f6XMZZTZzkyzYjgn0F5@dpg-co5en5fsc6pc7385cnvg-a/database_example_lff5")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
